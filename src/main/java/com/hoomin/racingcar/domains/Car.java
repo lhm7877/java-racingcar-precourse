cb42fc13673f857c424e29aaec14554ccd4a869f
lhm7877 <vkfdldh7>
2021-05-02 21:51:50 +0900

    test: Car 생성 테스트 / feat: Car, Name 구현

---
+++ src/main/java/com/hoomin/racingcar/domains/Car.java
@@ -0,0 +1,10 @@
+package com.hoomin.racingcar.domains;
+
+public class Car {
+
+	private final Name name;
+
+	public Car(String name) {
+		this.name = new Name(name);
+	}
+}
---
+++ src/main/java/com/hoomin/racingcar/domains/Name.java
@@ -0,0 +1,14 @@
+package com.hoomin.racingcar.domains;
+
+public class Name {
+	private static final int MAX_SIZE = 5;
+
+	private String name;
+
+	public Name(String name) {
+		if (name == null || name.length() > MAX_SIZE) {
+			throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+		}
+		this.name = name;
+	}
+}
