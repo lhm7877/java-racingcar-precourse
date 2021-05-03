8526546281218b77fa5fef24eb05da5267e5e376
lhm7877 <vkfdldh7>
2021-05-04 01:11:50 +0900

    feat: InputView 구현

--- src/main/java/com/hoomin/racingcar/RacingCarGame.java
+++ src/main/java/com/hoomin/racingcar/RacingCarGame.java
@@ -1,6 +1,7 @@
 package com.hoomin.racingcar;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.hoomin.racingcar.domains.Car;
 import com.hoomin.racingcar.domains.Cars;
@@ -8,12 +9,32 @@ import com.hoomin.racingcar.domains.Name;
 import com.hoomin.racingcar.domains.Racing;
 import com.hoomin.racingcar.domains.RacingResult;
 import com.hoomin.racingcar.domains.TotalRound;
+import com.hoomin.racingcar.view.InputView;
 
+/**
+ * Cars를 만들고 racing을 시작하며 result를 print하는 Game에 대한 책임
+ *
+ */
 public class RacingCarGame {
 	public void start() {
-		Cars cars = new Cars(Arrays.asList(new Car(new Name("a")), new Car(new Name("b")), new Car(new Name("c"))));
-		Racing racing = new Racing(cars, new TotalRound(5));
+		final List<String> carNames = InputView.getCarNames();
+		Cars cars = new Cars(createCarsByCarNames(carNames));
+		Racing racing = new Racing(cars, getTotalRound());
 		final RacingResult racingResult = racing.start();
 		racingResult.print();
+	}
+
+	private List<Car> createCarsByCarNames(List<String> carNames) {
+		List<Car> cars = new ArrayList<>();
+		for (String carName : carNames) {
+			final Name name = new Name(carName);
+			final Car car = new Car(name);
+			cars.add(car);
+		}
+		return cars;
+	}
+
+	private TotalRound getTotalRound() {
+		return new TotalRound(InputView.getTotalRound());
 	}
 }
--- src/main/java/com/hoomin/racingcar/view/InputView.java
+++ src/main/java/com/hoomin/racingcar/view/InputView.java
@@ -1,7 +1,19 @@
 package com.hoomin.racingcar.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 	private static final Scanner scanner = new Scanner(System.in);
+
+	public static List<String> getCarNames() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		return Arrays.asList(scanner.nextLine().split(","));
+	}
+
+	public static Integer getTotalRound() {
+		System.out.println("시도할 횟수는 몇 회인가요?");
+		return scanner.nextInt();
+	}
 }
