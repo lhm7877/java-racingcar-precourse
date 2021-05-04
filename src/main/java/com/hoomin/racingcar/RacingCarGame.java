f555356046e1b9c54574ea0da4749925befa9762
lhm7877 <vkfdldh7>
2021-05-04 21:51:35 +0900

    refactor: 메소드 분리

--- src/main/java/com/hoomin/racingcar/RacingCarGame.java
+++ src/main/java/com/hoomin/racingcar/RacingCarGame.java
@@ -3,26 +3,27 @@ package com.hoomin.racingcar;
 import java.util.List;
 
 import com.hoomin.racingcar.domains.Cars;
-import com.hoomin.racingcar.domains.Name;
 import com.hoomin.racingcar.domains.Racing;
 import com.hoomin.racingcar.view.InputView;
 import com.hoomin.racingcar.view.OutputView;
 
 /**
- * Cars를 만들고 racing을 시작하며 result를 print하는 Game에 대한 책임
+ * Cars를 만들고 racing을 시작하며 result(게임 결과)를 print하는 Game에 대한 책임
  *
  */
 public class RacingCarGame {
 	public void start() {
 		final List<String> carNames = InputView.getCarNames();
-		Cars cars = Cars.fromCarNames(carNames);
-		Racing racing = new Racing(cars, getTotalRound());
-		Cars winnerCars = racing.start();
-		final List<Name> names = winnerCars.getNames();
-		OutputView.printWinners(names);
+		final Integer totalRound = InputView.getTotalRound();
+
+		Cars winnerCars = getWinnerCars(carNames, totalRound);
+
+		OutputView.printWinners(winnerCars.getNames());
 	}
 
-	private Integer getTotalRound() {
-		return InputView.getTotalRound();
+	private Cars getWinnerCars(List<String> carNames, Integer totalRound) {
+		Cars cars = Cars.fromCarNames(carNames);
+		Racing racing = new Racing(cars, totalRound);
+		return racing.start();
 	}
 }
